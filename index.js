183afa5164b6aa5ae77d10065296c540f56bf186
winixt <haizekuo@gmail.com>
2024-01-12 12:55:17 +0800

    fix: handle process

--- index.js
+++ index.js
@@ -1,19 +1,19 @@
 // globalThis polyfill from https://mathiasbynens.be/notes/globalthis
 const getGlobalThis = () => {
-	if (typeof globalThis !== 'undefined') return globalThis;
-	if (typeof self !== 'undefined') return self;
-	if (typeof window !== 'undefined') return window;
-	if (typeof global !== 'undefined') return global;
-	if (typeof this !== 'undefined') return this;
-	throw new Error('Unable to locate global `this`');
+  if (typeof globalThis !== "undefined") return globalThis;
+  if (typeof self !== "undefined") return self;
+  if (typeof window !== "undefined") return window;
+  if (typeof global !== "undefined") return global;
+  if (typeof this !== "undefined") return this;
+  throw new Error("Unable to locate global `this`");
 };
 
-if (typeof process === "undefined") {
-	getGlobalThis().process = require("process/browser");
+if (typeof process === "undefined" || !process.cwd) {
+  getGlobalThis().process = require("@qlin/process/browser");
 }
 
 function getLinter() {
-	return require("./node_modules/eslint/lib/linter/linter").Linter;
+  return require("./node_modules/eslint/lib/linter/linter").Linter;
 }
 
 module.exports.Linter = getLinter();
